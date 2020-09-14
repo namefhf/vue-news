67ee8334a81fba12df1a8294ee7283499411cace
namefhf <2797822107@qq.com>
2020-09-14 20:36:34 +0800

    rem适配

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <h1>news</h1>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="warning">111</van-button>
+    <div class="wrap">wrap</div>
     <router-view />
   </div>
 </template>
@@ -9,4 +12,10 @@ export default {
   name: 'App'
 }
 </script>
-<style lang="less"></style>
+<style lang="less">
+.wrap {
+  width: 375px;
+  height: 375px;
+  background-color: aquamarine;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入全局样式
 import '@/styles/index.less'
+// 加载vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 自动设置rem基准值
+import 'amfe-flexible'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
