053365b82402bce524f9e399ce822700ddbf3aca
namefhf <2797822107@qq.com>
2020-09-27 09:05:18 +0800

    完成文章(取消)收藏

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -26,7 +26,7 @@
         :type="article.is_followed ? 'default' : 'danger'"
         class="follow-btn"
         :icon="article.is_followed ? '' : 'plus'"
-        :loading="isLoading"
+        :loading="isFollowLoading"
         @click="onFollow"
         >{{ article.is_followed ? '已关注' : '关注' }}</van-button
       >
@@ -34,14 +34,28 @@
     <div class="markdown-body">
       <div v-html="article.content" ref="article-content"></div>
     </div>
+    <div class="article-bottom">
+      <van-button type="default" round szie="small" class="comment-btn"
+        >写评论</van-button
+      >
+      <van-icon name="comment-o" info="123" />
+      <van-icon
+        :color="article.is_collected ? 'red' : ''"
+        :name="article.is_collected ? 'star' : 'star-o'"
+        @click="onCollect"
+      />
+      <van-icon name="good-job-o" />
+      <van-icon name="share" />
+    </div>
   </div>
 </template>
 
 <script>
-import { getArticleById } from '@/api/article'
+import { getArticleById, addCollect, deleteCollect } from '@/api/article'
 import './github-markdown.css'
 import { ImagePreview } from 'vant'
 import { addFollow, deleteFollow } from '@/api/user'
+
 export default {
   name: 'AticleIndex',
   props: {
@@ -53,7 +67,8 @@ export default {
   data () {
     return {
       article: {},
-      isLoading: false
+      isFollowLoading: false, // 关注loading
+      isCollectLoading: false // 收藏loading
     }
   },
   created () {
@@ -84,14 +99,29 @@ export default {
     },
     // 关注、取关用户
     async onFollow () {
-      this.isLoading = true
+      this.isFollowLoading = true
       if (this.article.is_followed) {
         await deleteFollow(this.article.aut_id)
       } else {
         await addFollow(this.article.aut_id)
       }
-      this.isLoading = false
+      this.isFollowLoading = false
       this.article.is_followed = !this.article.is_followed
+    },
+    // 收藏
+    async onCollect () {
+      // console.log('click')
+      this.$toast.loading({
+        message: '操作中....',
+        forbidclick: true
+      })
+      if (this.article.is_collected) {
+        await deleteCollect(this.article.art_id)
+      } else {
+        await addCollect(this.article.art_id)
+      }
+      this.article.is_collected = !this.article.is_collected
+      this.$toast.success(`${this.article.is_collected ? '' : '取消'}收藏成功`)
     }
   }
 }
@@ -124,6 +154,17 @@ export default {
   .markdown-body {
     padding: 14px;
     background-color: #fff;
+  }
+  .article-bottom {
+    background-color: #fff;
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    .comment-btn {
+      width: 150px;
+      height: 30px;
+    }
   }
 }
 </style>
