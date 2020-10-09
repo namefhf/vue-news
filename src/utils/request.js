4fec5930e20eff2744e9e970936709427fb07f1c
namefhf <2797822107@qq.com>
2020-10-09 09:49:18 +0800

    统一处理请求失败

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import store from '@/store/'
 import JSONbig from 'json-bigint'
+import { Toast } from 'vant'
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/',
   transformResponse: [
@@ -24,4 +25,25 @@ request.interceptors.request.use(config => {
   }
   return config
 })
+request.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    const status = error.response.status
+    if (status === 400) {
+      // 客户端参数错误
+      Toast.fail('客户端请求参数异常')
+    } else if (status === 401) {
+      // token无效
+    } else if (status === 403) {
+      // 无权限
+      Toast.fail('无权限操作')
+    } else if (status >= 500) {
+      // 服务端错误
+      Toast.fail('服务端异常')
+    }
+    return Promise.reject(error)
+  }
+)
 export default request
